ca56b68488e42af11eaf77444a195b603de1f614
mikeleo03 <leonmichael463@gmail.com>
2025-04-09 22:54:01 +0700

    [Chore] Detail comments

--- elderly.py
+++ elderly.py
@@ -1,18 +1,20 @@
 from mpyc.runtime import mpc
 
 async def main():
-    secint = mpc.SecInt(16)
+    secint = mpc.SecInt(16)  # Secure types
 
     await mpc.start()
 
-    my_age = int(input('Enter your age: '))
-    our_ages = mpc.input(secint(my_age))
+    my_age = int(input('Enter your age: '))     # Each party enters a number
+    our_ages = mpc.input(secint(my_age))        # List with one secint per party
 
-    total_age = sum(our_ages)
+    # Secure computation
+    total_age = mpc.sum(our_ages)
     max_age = mpc.max(our_ages)
     m = len(mpc.parties)
     above_avg = mpc.sum(age * m > total_age for age in our_ages)
 
+    # Secure output
     print('Average age:', await mpc.output(total_age) / m)
     print('Maximum age:', await mpc.output(max_age))
     print('Number of "elderly":', await mpc.output(above_avg))
